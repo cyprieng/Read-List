d749aae18e660982cb49437fbdc8e116bddcd1b0
Cyprien G <cyprien.guillemot@gmail.com>
2013-08-04 11:42:31 +0200

    Add config file

---
+++ global/config.php
@@ -0,0 +1,10 @@
+<?php
+	//Directories
+	define("MODELE_PATH",		"model/");
+	define("CONTROLLER_PATH",	"controller/");
+	define("VIEW_PATH",		"view/");
+	define("GLOBAL_PATH",		"global/");
+	define("IMAGE_PATH",		"assets/img/");
+	define("CSS_PATH",		"assets/css/");
+	define("JS_PATH",			"assets/js/");
+?>
